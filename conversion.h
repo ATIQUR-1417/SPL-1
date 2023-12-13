1c05cfb91fbcea3c5d9bdf23cd16112c7fb1d7e1
ATIQUR-1417 <144276116+ATIQUR-1417@users.noreply.github.com>
2023-12-13 21:33:24 +0600

    Update conversion.h

--- conversion.h
+++ conversion.h
@@ -1,21 +1,6 @@
 #ifndef CONVERSION_H
 #define CONVERSION_H
 
-/*string decToBin(int n);
-string decToOct(int n);
-string decToHex(int n);
-
-int binToDec(string s);
-string binToOct(string s);
-string binToHex(string s);
-
-int octToDec(string s);
-string octToBin(string s);
-string octToHex(string s);
-
-int hexToDec(string s);
-string hexToBin(string s);
-string hexToOct(string s);*/
 
 int conv();
 
